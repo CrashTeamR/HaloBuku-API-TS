e061a3b4d05ccd9918bfa3d42f42250710489da6
Eric Pradana <ericprd_@outlook.com>
2022-10-16 03:55:19 +0700

    fix cannot read process.env

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,35 @@
 import express, { Request, Response, json } from "express";
 import dotenv from "dotenv";
-
-const app = express();
+import router from "./routes";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+declare const process: {
+  env: {
+    DB_URI: string;
+  };
+};
+
+try {
+  mongoose.connect(process.env.DB_URI, () => {
+    console.log("connected to database");
+  });
+} catch (error) {
+  console.log(error);
+}
+const app = express();
 
 app.use(json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("HEllo");
+  res.status(200).send("Hello, this is root for HaloBuku API");
 });
 
-app.listen(4000, () => {
-  console.log("http://localhost:4000");
+app.use("/api/v1/", router);
+
+app.listen(5500, () => {
+  console.log("http://localhost:5500");
 });
 
 export default app;
