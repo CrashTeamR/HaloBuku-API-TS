70597c2e67d631d6d96d27c453aec52199b31c46
Eric Pradana <ericprd_@outlook.com>
2022-10-16 15:51:12 +0700

    add books router

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import authController from "./controllers/auth-controllers";
+import bookContollers from "./controllers/book-controller";
+import verifyToken from "./libs/verify-token";
 
 const router = Router();
 
 router.post("/register", authController.register);
 router.post("/verify", authController.verify);
 router.post("/login", authController.login);
+
+router.post("/books", verifyToken, bookContollers.addBook);
+router.get("/books", bookContollers.getAllBooks);
+router.get("/books/:id", bookContollers.getBookById);
 
 export default router;
